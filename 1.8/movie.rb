7376cbd7edbf330fbbf6034a07adb63d2e58d9c7
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2012-11-21 01:07:00 +0900

    p74

--- 1.8/movie.rb
+++ 1.8/movie.rb
@@ -1,34 +1,51 @@
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
+  attr_writer :price
 
-  attr_reader :title
-  attr_accessor :price_code
-
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title, the_price)
+    @title, self.price = title, the_price
   end
 
   def charge(days_rented)
-    result = 0
-    # 各行の金額を計算
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    # 新作２日間レンタルでボーナス点を加算
-    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
 
+class RegularPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+end
+
+class ChildrensPrice
+  include DefaultPrice
+
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
